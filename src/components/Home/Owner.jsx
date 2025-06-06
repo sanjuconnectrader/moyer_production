ce7f62df8a85b5c598200a19bfd7c77368f8d7a2
sanjuconnectrader <sanjith@connectrader.io>
2025-06-06 13:30:57 +0530

    Fixed Owner.jsx before pulling

--- src/components/Home/Owner.jsx
+++ src/components/Home/Owner.jsx
@@ -6,7 +6,7 @@ const Owner = () => (
     <section className="owner">
       {/* Text Column */}
       <div className="owner__copy">
-        <h2 className="owner__title">Moyer Production Co llC</h2>
+        <h2 className="owner__title">Moyer Production's Co LLC</h2>
         <div className="owner__underline"></div>
         
         <p className="owner__description">
